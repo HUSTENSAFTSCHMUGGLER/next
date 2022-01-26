c1166a195f91ac5de31472d17fc3b96ea8c44deb
HUSTENSAFTSCHMUGGLER <88559755+HUSTENSAFTSCHMUGGLER@users.noreply.github.com>
2022-01-26 18:51:48 +0100

    Tutorial

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,10 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import Head from 'next/head'
-import Script from 'next/script'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
@@ -15,19 +14,6 @@ export default function FirstPost() {
                     <a>Back to Home</a>
                 </Link>
             </h2>
-            <Image
-                src="/logo.png"
-                height={144}
-                width={144}
-                alt='A LLOTAN logo'        
-            />
-            <Script
-                src='https://connect.facebook.net/en_US/sdk.js'
-                strategy='lazyOnload'
-                onLoad={() =>
-                    console.log('script loadet')
-                }
-            />
-        </>
+        </Layout>
     )
 }
