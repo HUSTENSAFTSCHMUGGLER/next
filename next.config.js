88cf2efea46c7f3999c125959a4a6b18531a12c1
HUSTENSAFTSCHMUGGLER <88559755+HUSTENSAFTSCHMUGGLER@users.noreply.github.com>
2022-01-26 19:32:18 +0100

    next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
-    images: {
-        loader: 'akamai',
-        path: '',
-    },
-}
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+module.exports = withPlugins([
+    [optimizedImages, {
+        /* config for next-optimized-images */
+    }],
+
+    // your other plugins here
+
+]);
