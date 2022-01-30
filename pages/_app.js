2a298f45048f0b9e570099287369f615e59f8b44
HUSTENSAFTSCHMUGGLER <88559755+HUSTENSAFTSCHMUGGLER@users.noreply.github.com>
2022-01-30 17:03:18 +0100

    Console Log Easteregg

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+﻿import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import Head from 'next/head'
 
@@ -6,6 +6,9 @@ function MyApp({ Component, pageProps }) {
     return (
         <SessionProvider>
             <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous" />
             </Head>
