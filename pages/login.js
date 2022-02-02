2739477c53f621f12040686d028b7f91e5ff06eb
HUSTENSAFTSCHMUGGLER <88559755+HUSTENSAFTSCHMUGGLER@users.noreply.github.com>
2022-02-02 22:45:39 +0100

    Try Background

--- pages/login.js
+++ pages/login.js
@@ -23,54 +23,56 @@ const Login = ({ getProviders, getSession }) => {
 	}
     
 	return (
-		<div className="container" id="container">
-			<div className="form-container sign-up-container">
-				<form action="#">
-					<h1>Create Account</h1>
-					<div className="social-container flex">
-						<BtnLogin getProviders={getProviders.google} icon={faGooglePlus}/>
-						<BtnLogin getProviders={getProviders.facebook} icon={faFacebook}/>
-						<BtnLogin getProviders={getProviders.github} icon={faGithub}/>
-						<BtnLogin getProviders={getProviders.reddit} icon={faReddit}/>
-					</div>
-					<span>or use your email for registration</span>
-					<input type="text" placeholder="Name" />
-					<input type="email" placeholder="Email" />
-					<input type="password" placeholder="Password" />
-					<button className="loginButton loginext">Sign Up</button>
-				</form>
-			</div>
-			<div className="form-container sign-in-container">
-				<form action="#">
-					<h1>Sign in</h1>
-					<div className="social-container flex">
-						<BtnLogin getProviders={getProviders.google} icon={faGooglePlus}/>
-						<BtnLogin getProviders={getProviders.facebook} icon={faFacebook}/>
-						<BtnLogin getProviders={getProviders.github} icon={faGithub}/>
-						<BtnLogin getProviders={getProviders.reddit} icon={faReddit}/>
-					</div>
-					<span>or use your account</span>
-					<input type="email" placeholder="Email" />
-					<input type="password" placeholder="Password" />
-					<a href="#">Forgot your password?</a>
-					<button className="loginButton loginext">Sign In</button>
-				</form>
-			</div>
-			<div className="overlay-container">
-				<div className="overlay">
-					<div className="overlay-panel overlay-left">
-						<h1>Welcome Back!</h1>
-						<p>To keep connected with us please login with your personal info</p>
-						<button className="loginButton ghost loginext" onClick={handleSignIn} id="signIn">Sign In</button>
-					</div>
-					<div className="overlay-panel overlay-right">
-						<h1>Hello, Friend!</h1>
-						<p>Enter your personal details and start journey with us</p>
-						<button className="loginButton ghost loginext" onClick={handleSignUp} id="signUp">Sign Up</button>
+		<>
+			<div className="container" id="container">
+				<div className="form-container sign-up-container">
+					<form action="#">
+						<h1>Create Account</h1>
+						<div className="social-container flex">
+							<BtnLogin getProviders={getProviders.google} icon={faGooglePlus}/>
+							<BtnLogin getProviders={getProviders.facebook} icon={faFacebook}/>
+							<BtnLogin getProviders={getProviders.github} icon={faGithub}/>
+							<BtnLogin getProviders={getProviders.reddit} icon={faReddit}/>
+						</div>
+						<span>or use your email for registration</span>
+						<input type="text" placeholder="Name" />
+						<input type="email" placeholder="Email" />
+						<input type="password" placeholder="Password" />
+						<button className="loginButton loginext">Sign Up</button>
+					</form>
+				</div>
+				<div className="form-container sign-in-container">
+					<form action="#">
+						<h1>Sign in</h1>
+						<div className="social-container flex">
+							<BtnLogin getProviders={getProviders.google} icon={faGooglePlus}/>
+							<BtnLogin getProviders={getProviders.facebook} icon={faFacebook}/>
+							<BtnLogin getProviders={getProviders.github} icon={faGithub}/>
+							<BtnLogin getProviders={getProviders.reddit} icon={faReddit}/>
+						</div>
+						<span>or use your account</span>
+						<input type="email" placeholder="Email" />
+						<input type="password" placeholder="Password" />
+						<a href="#">Forgot your password?</a>
+						<button className="loginButton loginext">Sign In</button>
+					</form>
+				</div>
+				<div className="overlay-container">
+					<div className="overlay">
+						<div className="overlay-panel overlay-left">
+							<h1>Welcome Back!</h1>
+							<p>To keep connected with us please login with your personal info</p>
+							<button className="loginButton ghost loginext" onClick={handleSignIn} id="signIn">Sign In</button>
+						</div>
+						<div className="overlay-panel overlay-right">
+							<h1>Hello, Friend!</h1>
+							<p>Enter your personal details and start journey with us</p>
+							<button className="loginButton ghost loginext" onClick={handleSignUp} id="signUp">Sign Up</button>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div >
+		</>
     )
 }
 
