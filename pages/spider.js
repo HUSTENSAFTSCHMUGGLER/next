ae14502987de60c875008ba5a87e972123a4a800
Florian Saathoff <88559755+floriansaathoff@users.noreply.github.com>
2022-11-04 10:57:31 +0100

    Delete spider.js

--- pages/spider.js
+++
@@ -1,7 +0,0 @@
-﻿export default function Home({ isConnected }) {
-  return (
-    <div>
-          <iframe src="https://861102111.tapecontent.net/radosgw/zMlYD24aw4CYlbb/MJ8DhGkL0-cNk8lUaXWjinR9rGLmOk287rfOz6b0FW4PO6tHXOkudsE3eZiRxDZcQsgr-_b__Bije6aInBJtwMHwmhOzcYQjCcK0xkF-GcTWOs3SFRjZ6FP5zeu1S3NcsHz5OkhKlYCFnT-EIUNfyELx9-aF_iyG6RFAGzkfDLfiU5B_DRwOuArWMC02bFGnN1L3oIXli1OT5hA4LmLYjdPeHvX799VOxvX75lS5KpTzRn0DvRDekXxfRrSgUNkMgmlqeeO3HsvK-sJ7DzS05lMDcMxmbv85L83guw/prd-spider-bd-sd.mkv.mp4?stream=1" width="800" height="600" allowfullscreen="true" allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>
-    </div>
-  )
-}
